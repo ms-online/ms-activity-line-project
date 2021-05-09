70d879c2be0d247e469c746b24732c8f74b4d719
ms-online <27732357@qq.com>
2021-05-09 18:18:09 +0800

    lesson-11

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,19 +1,32 @@
 import Button from '../ui/button'
 import classes from './events-search.module.css'
+import { useRef } from 'react'
 function EventsSearch(props) {
+  const yearInputRef = useRef()
+  const monthInputRef = useRef()
+  // 提交表单（获取年份和月份信息）
+  function submitHandler(event) {
+    event.preventDefault()
+
+    //获取DOM中的年份和月份值
+    const selectedYear = yearInputRef.current.value
+    const selectedMonth = monthInputRef.current.value
+
+    props.onSearch(selectedYear, selectedMonth)
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor='year'>年</label>
-          <select id='year'>
+          <select id='year' ref={yearInputRef}>
             <option value='2021'>2021</option>
             <option value='2022'>2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor='month'>月</label>
-          <select id='month'>
+          <select id='month' ref={monthInputRef}>
             <option value='1'>1月</option>
             <option value='2'>2月</option>
             <option value='3'>3月</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,19 @@
 import { getAllEvents } from '../../dummy-data'
+import { useRouter } from 'next/router'
 import EventList from '../../components/events/event-list'
 import EventsSearch from '../../components/events/events-search'
 import { Fragment } from 'react'
 function AllEventsPage() {
+  const router = useRouter()
   const events = getAllEvents()
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   )
