7f9a8c5046a0d1e257a3c0540857d9d6e8902b73
ms-online <27732357@qq.com>
2021-05-14 15:21:24 +0800

    lesson-17

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,12 @@
-import { getAllEvents } from '../../dummy-data'
+import { getAllEvents } from '../../helpers/api-util'
 import { useRouter } from 'next/router'
 import EventList from '../../components/events/event-list'
 import EventsSearch from '../../components/events/events-search'
 import { Fragment } from 'react'
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter()
-  const events = getAllEvents()
+
+  const { events } = props
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`
@@ -20,3 +21,14 @@ function AllEventsPage() {
 }
 
 export default AllEventsPage
+
+export async function getStaticProps() {
+  const events = await getAllEvents()
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  }
+}
