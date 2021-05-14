5e32fb1ac2c26ffe8f4be0f0f40c7c5c4fc633a5
ms-online <27732357@qq.com>
2021-05-14 17:06:39 +0800

    lesson-19

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,5 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import useSWR from 'swr'
 import { useRouter } from 'next/router'
 import { getFilteredEvents } from '../../helpers/api-util'
 import EventList from '../../components/events/event-list'
@@ -6,24 +7,49 @@ import ResultsTitle from '../../components/events/results-title'
 import Button from '../../components/ui/button'
 import ErrorAlert from '../../components/ui/error-alert'
 function FilteredEventPage(props) {
-  // const router = useRouter()
+  const [loadedEvents, setLoadedEvent] = useState()
+  const router = useRouter()
+  const filterData = router.query.slug
 
-  // const filterData = router.query.slug
-  // console.log(filterData)
+  const { data, error } = useSWR(
+    'https://nextjs-course-36511-default-rtdb.firebaseio.com/events.json'
+  )
 
-  // if (!filterData) {
-  //   return <p className='center'>页面加载中...</p>
-  // }
+  useEffect(() => {
+    if (data) {
+      const events = []
 
-  // //处理捕获的路由参数
-  // const filteredYear = filterData[0]
-  // const filteredMonth = filterData[1]
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        })
+      }
+      setLoadedEvent(events)
+    }
+  }, [data])
 
-  // const numYear = +filteredYear
-  // const numMonth = +filteredMonth
+  if (!loadedEvents) {
+    return <p className='center'>页面加载中...</p>
+  }
+
+  //处理捕获的路由参数
+  const filteredYear = filterData[0]
+  const filteredMonth = filterData[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
 
   //判断是否为有效年份
-  if (props.hasError) {
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2022 ||
+    numYear < 2021 ||
+    numMonth > 12 ||
+    numMonth < 1 ||
+    error
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -37,7 +63,13 @@ function FilteredEventPage(props) {
     )
   }
 
-  const filteredEvents = props.filteredEvents
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date)
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    )
+  })
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -54,7 +86,7 @@ function FilteredEventPage(props) {
   }
 
   //日期格式化
-  const date = new Date(props.date.year, props.date.month - 1)
+  const date = new Date(numYear, numMonth - 1)
   return (
     <div>
       <ResultsTitle date={date} />
@@ -65,49 +97,49 @@ function FilteredEventPage(props) {
 
 export default FilteredEventPage
 
-export async function getServerSideProps(context) {
-  const { params } = context
-  const filterData = params.slug
+// export async function getServerSideProps(context) {
+//   const { params } = context
+//   const filterData = params.slug
 
-  //处理捕获的路由参数
-  const filteredYear = filterData[0]
-  const filteredMonth = filterData[1]
+//   //处理捕获的路由参数
+//   const filteredYear = filterData[0]
+//   const filteredMonth = filterData[1]
 
-  const numYear = +filteredYear
-  const numMonth = +filteredMonth
+//   const numYear = +filteredYear
+//   const numMonth = +filteredMonth
 
-  //判断是否为有效年份
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2022 ||
-    numYear < 2021 ||
-    numMonth > 12 ||
-    numMonth < 1
-  ) {
-    return {
-      props: {
-        hasError: true,
-      },
-      // notFound: true,
-      // redirect: {
-      //   destination:'/error'
-      // }
-    }
-  }
+//   //判断是否为有效年份
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2022 ||
+//     numYear < 2021 ||
+//     numMonth > 12 ||
+//     numMonth < 1
+//   ) {
+//     return {
+//       props: {
+//         hasError: true,
+//       },
+//       // notFound: true,
+//       // redirect: {
+//       //   destination:'/error'
+//       // }
+//     }
+//   }
 
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  })
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   })
 
-  return {
-    props: {
-      filteredEvents: filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth,
-      },
-    },
-  }
-}
+//   return {
+//     props: {
+//       filteredEvents: filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth,
+//       },
+//     },
+//   }
+// }
