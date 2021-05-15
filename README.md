bfe8ee3dd8be40f66f07d27148a7f9861bceab4a
ms-online <27732357@qq.com>
2021-05-15 15:12:16 +0800

    lesson-25

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-lesson-24:使用\_app.js 文件实现重复逻辑
+lesson-25:合并 head 内容
--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>活动行</title>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <Component {...pageProps} />
